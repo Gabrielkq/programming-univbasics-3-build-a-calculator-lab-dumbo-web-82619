fdc64a855e8c0006405d5486bfa6e32b6e118f4c
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-06 21:32:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ return sum
 end
 
 def subtraction(num1, num2)
-
+diff = num1 - num2
+return diff
 end
 
 def division(num1, num2)
-
+quot = num1 / num2
+return quot
 end
 
 def multiplication(num1, num2)
-
+product = num1*num2
+return product
 end
 
 def modulo(num1, num2)
-
+remain = num1%num2
+return remain
 end
 
 def square_root(num)
-
+root = Math.sqrt(num)
+return root
 end
